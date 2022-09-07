04c042cc40b9fc8d3dfbd86d63328a54ecee8ec3
Tomas Tripodi <112105280+totripodi@users.noreply.github.com>
2022-09-07 19:19:18 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,3 +9,19 @@ if aux%2==0:
     print("es par")
 else:
     print("es impar")
+    
+    
+    #Sin contadores
+    
+par=True
+n=int(input("ingrese numero")
+ while n!=-1
+      if par:
+        par=false
+      else:
+        par=true
+      n=int(input("ingrese numero")
+ if par:
+    print("la cant de numeros son par")
+ else:
+    print ("la cantidad de numeros es impar")
